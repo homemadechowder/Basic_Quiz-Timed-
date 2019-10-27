b4a2199c030a83b0e2a7a17d48ee38c5986fd75c
Andrew Lu <awclu@ucdavis.edu>
2019-10-27 12:57:23 -0700

    added html

---
+++ assets/script/script.js
@@ -0,0 +1,22 @@
+
+//Ready the document
+$( document ).ready(function() {
+    var answerArr = ["A,B,A,D,C,B,D,A,D,C,B"];
+    //Answers:
+    //Plankton: B
+    //Atlantic Ocean: A
+    //Pelagic Zone: D
+    //1500ft: C
+    //Pacific: B
+    //Archipelago: D
+    //Sailfish: A
+    //Near the Coast: D
+    //Salt: C
+    //Ocean Sunfish: B    
+
+
+});
+
+
+
+
